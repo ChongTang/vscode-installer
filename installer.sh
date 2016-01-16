93ab94fa8c8b66c42878f47a77163c2f0973b597
Chong Tang <ctang@virginia.edu>
2016-01-15 21:09:48 -0500

    update desktop file

--- installer.sh
+++ installer.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
+
 echo "Thank you for using this installer to install Visual Studio Code!"
 
-home_path=`echo $HOME`
+home_path=$HOME
 # default install path
-default_path="$home_path/.local/share/visual-studio-code"
-input_path=""
-# ask the user to input path
-read -p "Install path [$default_path]: " input_path
-if [[ -z "$input_path" ]]
-then
-  input_path=$default_path
-fi
-echo "Install to $input_path"
-mkdir -p $input_path
+input_path="$home_path/.local/share/visual-studio-code"
+mkdir -p "$input_path"
+rm -rf "$input_path/VSCode-linux-x64"
 
-echo "Download the installation package now..."
+printf "Download the installation package now...\n"
 # download the package to installation folder
 wget https://github.com/ChongTang/vscode-installer/raw/master/package/VSCode-linux64.zip -O VSCode.zip
-unzip VSCode.zip -d $input_path
+unzip -qq VSCode.zip -d "$input_path"
 
 # create a desktop file
-desktop_entry=$'[Desktop Entry]\nType=Application\nEncoding=UTF-8\nName=Visual Studio Code\nComment=Visual Studio Code IDE\nExec=$desktop_entry$input_path/VSCode-linux-x64/Code\nIcon=$desktop_entry$input_path/VSCode-linux-x64/resources/app/resources/linux/vscode.png\nTerminal=false'
+desktop_entry="[Desktop Entry]\nName=Visual Studio Code\nComment=Visual Studio focused on modern web and cloud\nType=Application\nCategories=Development;\nEncoding=UTF-8\nExec=$desktop_entry$input_path/VSCode-linux-x64/Code\nIcon=$desktop_entry$input_path/VSCode-linux-x64/resources/app/resources/linux/vscode.png\nTerminal=false"
 
 # store desktop file in to local applications folder
-echo "$desktop_entry" > ~/.local/share/applications/VSCode.desktop
-echo "Installation finished. Enjoy!'
+echo -e "$desktop_entry" > ~/.local/share/applications/visual-studio-code.desktop
+echo "Installation finished. Enjoy!"
